6e0c50fb0d876381d476e7fd97c000551ac13bce
nexrom <mr@nexrom.de>
2021-09-11 15:57:36 +0200

    revision, new structure

--- vhdxParser.cs
+++ vhdxParser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.ComponentModel;
 
-namespace Common
+namespace vhdxParser
 {
     public class vhdxParser : IDisposable
     {
@@ -14,34 +14,7 @@ namespace Common
 
         public vhdxParser(string file)
         {
-            try
-            {
-                this.sourceStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
-
-        //public static function to just retrieve virtual disk id from a given vhdx file
-        public static byte[] getVHDXIDFromFile(string file)
-        {
-            //open vhdx file within parser
-            vhdxParser parser = new vhdxParser(file);
-            Common.RegionTable regionTable = parser.parseRegionTable();
-
-            //return nothing if regionTable contains of no entries
-            if (regionTable.entries.Length == 0)
-            {
-                parser.close();
-                return null;
-            }
-
-            Common.MetadataTable metadataTable = parser.parseMetadataTable(regionTable);
-            byte[] id = parser.getVirtualDiskID(metadataTable);
-            parser.close();
-            return id;
+            this.sourceStream = new FileStream(file, FileMode.Open, FileAccess.Read);
 
         }
 
@@ -51,8 +24,134 @@ namespace Common
             this.sourceStream.Close();
         }
 
+        //parses the whole vhdx meta data
+        public VHDXData parse()
+        {
+            VHDXData dataStruct = new VHDXData();
+            dataStruct.fileHeader = new FileHeader();
+
+            //read raw file header section
+            dataStruct.fileHeader.rawFileHeader = getRawHeader();
+
+            //parse file identifier
+
+            //signature
+            dataStruct.fileHeader.fileIdentifier.signature = System.Text.Encoding.ASCII.GetString(dataStruct.fileHeader.rawFileHeader, 0, 8);
+
+            //creator
+            dataStruct.fileHeader.fileIdentifier.creator = System.Text.Encoding.Unicode.GetString(dataStruct.fileHeader.rawFileHeader, 8, 512);
+
+            //parse header 1
+            dataStruct.fileHeader.header1 = parseHeader(dataStruct.fileHeader.rawFileHeader, 1);
+
+            //parse header 2
+            dataStruct.fileHeader.header2 = parseHeader(dataStruct.fileHeader.rawFileHeader, 2);
+
+            //parse reagionTable 1
+            dataStruct.fileHeader.regionTable1 = parseRegionTable(1);
+
+            //parse reagionTable 2
+            dataStruct.fileHeader.regionTable2 = parseRegionTable(2);
+
+            //parse metadataTable
+            dataStruct.metadataTable = parseMetadataTable(dataStruct.fileHeader.regionTable1);
+
+
+            //parse various values
+            byte[] virtualDiskID = getVirtualDiskID(dataStruct.metadataTable);
+            dataStruct.virtualDiskID = virtualDiskID;
+            ulong virtualDiskSize = getVirtualDiskSize(dataStruct.metadataTable);
+            dataStruct.virtualDiskSize = virtualDiskSize;
+            uint logicalSectorSize = getLogicalSectorSize(dataStruct.metadataTable);
+            dataStruct.logicalSectorSize = logicalSectorSize;
+            uint blockSize = getBlockSize(dataStruct.metadataTable);
+            dataStruct.blockSize = blockSize;
+
+            //calculate chunkSize
+            UInt32 chunkSize = (UInt32)((Math.Pow(2, 32) * logicalSectorSize) / blockSize);
+            dataStruct.chunkRatio = chunkSize;
+
+            //parse batTable
+            dataStruct.batTable = parseBATTable(dataStruct.fileHeader.regionTable1, chunkSize, 0, false);
+
+            //get raw log
+            dataStruct.rawLog = getRawLog();
+
+
+            return dataStruct;
+        }
+
+
+        //parses header by the given numer (1 or 2)
+        private Header parseHeader(byte[] fileHeader, byte headerNumber)
+        {
+            Header header = new Header();
+            Int32 currentOffset = 64 * headerNumber * 1024; //64KB * 1024
+
+            //read signature
+            string signature = System.Text.Encoding.ASCII.GetString(fileHeader, currentOffset, 4);
+            header.signature = signature;
+            currentOffset += 4;
+
+            //read checksum
+            byte[] checksum = new byte[4];
+            Array.Copy(fileHeader, currentOffset, checksum, 0, 4);
+            header.checksum = checksum;
+            currentOffset += 4;
+
+            //read sequenceNumber
+            UInt64 sequenceNumber = BitConverter.ToUInt64(fileHeader, currentOffset);
+            header.sequenceNumber = sequenceNumber;
+            currentOffset += 8;
+
+            //read FileWriteGuid
+            byte[] fileWriteGuid = new byte[16];
+            Array.Copy(fileHeader, currentOffset, fileWriteGuid, 0, 16);
+            header.fileWriteGuid = fileWriteGuid;
+            currentOffset += 16;
+
+            //read DataWriteGuid
+            byte[] dataWriteGuid = new byte[16];
+            Array.Copy(fileHeader, currentOffset, dataWriteGuid, 0, 16);
+            header.dataWriteGuid = dataWriteGuid;
+            currentOffset += 16;
+
+            //read LogGuid
+            byte[] logGuid = new byte[16];
+            Array.Copy(fileHeader, currentOffset, logGuid, 0, 16);
+            header.logGuid = logGuid;
+            currentOffset += 16;
+
+            //read LogVersion
+            UInt16 logVersion;
+            logVersion = BitConverter.ToUInt16(fileHeader, currentOffset);
+            header.logVersion = logVersion;
+            currentOffset += 2;
+
+            //read Version
+            UInt16 version;
+            version = BitConverter.ToUInt16(fileHeader, currentOffset);
+            header.version = version;
+            currentOffset += 2;
+
+            //read LogLength
+            UInt32 logLength;
+            logLength = BitConverter.ToUInt32(fileHeader, currentOffset);
+            header.logLength = logLength;
+            currentOffset += 4;
+
+            //read LogOffset
+            UInt64 logOffset;
+            logOffset = BitConverter.ToUInt32(fileHeader, currentOffset);
+            header.logOffset = logOffset;
+            currentOffset += 8;
+
+            return header;
+        }
+
+
         //gets the raw bat table
-        public RawBatTable getRawBatTable(RegionTable table)
+        private RawBatTable getRawBatTable(RegionTable table)
         {
             RawBatTable rawTable = new RawBatTable();
             UInt64 batOffset = 0;
@@ -80,23 +179,20 @@ namespace Common
         }
 
         //gets the raw header
-        public RawHeader getRawHeader()
+        private byte[] getRawHeader()
         {
-            RawHeader header = new RawHeader();
-            header.rawData = new byte[1048576]; // 1MB * 1024 * 1024
+            byte[] buffer = new byte[1048576]; // 1MB * 1024 * 1024
 
             //just return the first 1MB of data
             this.sourceStream.Seek(0, SeekOrigin.Begin);
-            this.sourceStream.Read(header.rawData, 0, header.rawData.Length);
+            this.sourceStream.Read(buffer, 0, buffer.Length);
 
-            return header;
+            return buffer;
         }
 
         //gets the raw log section
-        public RawLog getRawLog()
+        private byte[] getRawLog()
         {
-            RawLog rawLog = new RawLog();
-
             //jump the Header1
             this.sourceStream.Seek(65536, SeekOrigin.Begin); //64KB * 1024
 
@@ -119,15 +215,12 @@ namespace Common
             buffer = new byte[logLength];
             this.sourceStream.Read(buffer, 0, buffer.Length);
 
-            rawLog.rawData = buffer;
-            rawLog.logLength = logLength;
-            rawLog.vhdxOffset = logOffset;
 
-            return rawLog;
+            return buffer;
         }
 
         //reads blockSize from MetadataTable
-        public UInt32 getBlockSize(MetadataTable metadataTable)
+        private UInt32 getBlockSize(MetadataTable metadataTable)
         {
             UInt32 offset = 0;
             UInt32 length = 0;
@@ -151,7 +244,7 @@ namespace Common
         }
 
         //reads "virtual disk ID" from MetadataTable
-        public byte[] getVirtualDiskID(MetadataTable metadataTable)
+        private byte[] getVirtualDiskID(MetadataTable metadataTable)
         {
             UInt32 offset = 0;
             UInt32 length = 0;
@@ -175,7 +268,7 @@ namespace Common
         }
 
         //reads logicalSectorSize from MetadataTable
-        public UInt32 getLogicalSectorSize(MetadataTable metadataTable)
+        private UInt32 getLogicalSectorSize(MetadataTable metadataTable)
         {
             UInt32 offset = 0;
             UInt32 length = 0;
@@ -199,7 +292,7 @@ namespace Common
         }
 
         //reads virtualDiskSize from MetadataTable
-        public UInt64 getVirtualDiskSize(MetadataTable metadataTable)
+        private UInt64 getVirtualDiskSize(MetadataTable metadataTable)
         {
             UInt32 offset = 0;
             UInt32 length = 0;
@@ -222,49 +315,18 @@ namespace Common
             return BitConverter.ToUInt64(buffer, 0);
         }
 
-        //gets the raw MetadataTable
-        public RawMetadataTable getRawMetadataTable(RegionTable table)
-        {
-            RawMetadataTable rawMetadataTable = new RawMetadataTable();
-
-            MetadataTable metadataTable = new MetadataTable();
-            metadataTable.entries = new List<MetadataTableEntry>();
-            UInt64 metadataTableOffset = 0;
-            UInt32 metadataTableLength = 0;
-
-            //read offset and length for BAT table
-            foreach (RegionTableEntry entry in table.entries)
-            {
-                if (entry.guid[0] == 0x06)
-                {
-                    metadataTableOffset = entry.fileOffset;
-                    metadataTableLength = entry.length;
-                    break;
-                }
-            }
-
-            rawMetadataTable.length = metadataTableLength;
-            rawMetadataTable.vhdxOffset = metadataTableOffset;
-
-            //read whole table
-            this.sourceStream.Seek((long)metadataTableOffset, SeekOrigin.Begin);
-            byte[] buffer = new byte[metadataTableLength];
-            this.sourceStream.Read(buffer, 0, buffer.Length);
-            rawMetadataTable.rawData = buffer;
-
-            return rawMetadataTable;
-        }
+       
 
 
         //parses the metadata region
-        public MetadataTable parseMetadataTable(RegionTable table)
+        private MetadataTable parseMetadataTable(RegionTable table)
         {
             MetadataTable metadataTable = new MetadataTable();
             metadataTable.entries = new List<MetadataTableEntry>();
             UInt64 metadataTableOffset = 0;
             UInt32 metadataTableLength = 0;
 
-            //read offset and length for BAT table
+            //read offset and length for meta table
             foreach (RegionTableEntry entry in table.entries)
             {
                 if (entry.guid[0] == 0x06)
@@ -275,7 +337,7 @@ namespace Common
                 }
             }
 
-            //jump to first BAT entry
+            //jump to first meta entry
             this.sourceStream.Seek((long)metadataTableOffset, SeekOrigin.Begin);
             byte[] buffer = new byte[metadataTableLength];
             this.sourceStream.Read(buffer, 0, buffer.Length);
@@ -283,13 +345,15 @@ namespace Common
             MetadataTableHeader metadataTableHeader = new MetadataTableHeader();
 
             //read signature
-            metadataTableHeader.signature = Encoding.UTF8.GetString(buffer, 0, 8);
+            metadataTableHeader.signature = Encoding.ASCII.GetString(buffer, 0, 8);
 
             //read reserved
             metadataTableHeader.reserved = BitConverter.ToUInt16(buffer, 8);
 
             //read entry count
             metadataTableHeader.entryCount = BitConverter.ToUInt16(buffer, 10);
+
+            metadataTable.header = metadataTableHeader;
 
             UInt32 entryOffset = 32;
             //iterate through all entries
@@ -317,7 +381,7 @@ namespace Common
 
 
         //parses the BAT table (chunkSize just necessary when removeSectorMask is set)
-        public BATTable parseBATTable(RegionTable table, UInt32 chunkSize, UInt32 sectorBitmapBlocksCount, bool removeSectorMask)
+        private BATTable parseBATTable(RegionTable table, UInt32 chunkSize, UInt32 sectorBitmapBlocksCount, bool removeSectorMask)
         {
             BATTable batTable = new BATTable();
             batTable.entries = new List<BATEntry>();
@@ -340,10 +404,9 @@ namespace Common
 
             //read whole table
             byte[] buffer = new byte[batLength];
-            int bytesRead = this.sourceStream.Read(buffer, 0, (int)batLength);
-            if (bytesRead != batLength)
-            {
-            }
+            this.sourceStream.Read(buffer, 0, (int)batLength);
+
+            batTable.rawBatTable = buffer;
 
             //each entry consists of 64bit, iterate
             UInt32 entryCount = batLength / 64;
@@ -381,21 +444,21 @@ namespace Common
             return batTable;
         }
 
-        //
+        
 
-        //parses the region table
-        public RegionTable parseRegionTable()
+        //parses the region table by the given number (1 or 2)
+        private RegionTable parseRegionTable(byte number)
         {
-            //file opened?
-            if (this.sourceStream == null)
+            if (number == 1)
             {
-                RegionTable dummyRegionTable = new RegionTable();
-                dummyRegionTable.isValid = false;
-                return dummyRegionTable;
+                //set pointer to RegionTable 1
+                this.sourceStream.Seek(192 * 1024, SeekOrigin.Begin);
             }
-
-            //set pointer to RegionTable 1
-            this.sourceStream.Seek(192 * 1024, SeekOrigin.Begin);
+            else
+            {
+                //set pointer to RegionTable 2
+                this.sourceStream.Seek(256 * 1024, SeekOrigin.Begin);
+            }
 
             //reserve buffer
             int regionSize = 256 * 1024 - 192 * 1024;
@@ -442,7 +505,7 @@ namespace Common
                 entry.length = BitConverter.ToUInt32(buffer, entryByteOffset + 16 + 8);
 
                 //read required
-                entry.required = BitConverter.ToUInt32(buffer, entryByteOffset + 16 + 8 + 8);
+                entry.required = BitConverter.ToUInt32(buffer, entryByteOffset + 16 + 8 + 4);
 
                 entries[i] = entry;
                 entryByteOffset += 16 + 8 + 8;
@@ -457,11 +520,26 @@ namespace Common
             this.close();
         }
     }
+
+
+    public struct VHDXData
+    {
+        public FileHeader fileHeader;
+        public BATTable batTable;
+        public MetadataTable metadataTable;
+        public byte[] rawLog;
+        public UInt32 chunkRatio;
+        public UInt32 logicalSectorSize;
+        public UInt32 blockSize;
+        public UInt64 virtualDiskSize;
+        public byte[] virtualDiskID;
+    }
+
+
     public struct RegionTable
     {
         public RegionTableHeader header;
         public RegionTableEntry[] entries;
-        public bool isValid;
     }
 
     public struct RegionTableHeader
@@ -483,6 +561,7 @@ namespace Common
     public struct BATTable
     {
         public List<BATEntry> entries;
+        public byte[] rawBatTable;
     }
 
     public struct BATEntry
@@ -514,26 +593,40 @@ namespace Common
         public UInt32 length;
     }
 
-    public struct FileParametersTable
+    public struct FileHeader
     {
-        public UInt32 blockSize;
-        public UInt32 reserved;
+        public FileIdentifier fileIdentifier;
+        public Header header1;
+        public Header header2;
+        public RegionTable regionTable1;
+        public RegionTable regionTable2;
+        public byte[] rawFileHeader;
     }
+
+    public struct Header
+    {
+        public string signature;
+        public byte[] checksum;
+        public UInt64 sequenceNumber;
+        public byte[] fileWriteGuid;
+        public byte[] dataWriteGuid;
+        public byte[] logGuid;
+        public UInt16 logVersion;
+        public UInt16 version;
+        public UInt32 logLength;
+        public UInt64 logOffset;
+    }
+
+    public struct FileIdentifier
+    {
+        public string signature;
+        public string creator;
+    }
+
 
     public struct RawBatTable
     {
         public UInt64 vhdxOffset;
-        public byte[] rawData;
-    }
-
-    public struct RawHeader
-    {
-        public byte[] rawData;
-    }
-
-    public struct RawMetadataTable
-    {
-        public UInt64 vhdxOffset, length;
         public byte[] rawData;
     }
 
